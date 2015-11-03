1c845dde580d957786c348a2adbc00ef005c0a73
Gabriele Marazzi <gabriele.marazzi@gmail.com>
2015-11-03 21:50:08 +0100

    add method geometry

--- src/SchemaExtended/Blueprint.php
+++ src/SchemaExtended/Blueprint.php
@@ -10,6 +10,26 @@ use Illuminate\Database\Schema\Blueprint as IlluminateBlueprint;
  */
 class Blueprint extends IlluminateBlueprint {
 
+
+    /**
+    * Create a new Geometry column on the table.
+    *
+    * @param  string   $column
+    * @return \Illuminate\Support\Fluent
+    */
+    public function geometry($column, $type)
+    {
+        $spatial_types = ['point', 'linestring', 'polygon', 'multipoint', 'multilinestring', 'multipolygon', 'geometrycollection'];
+    
+        if ( ! in_array( $type, $spatial_types) )
+            throw new \Exception('Unknown geometry type: ' . $type);
+    
+        // necessario per non overridare type che è già usato nel namespace chiamante
+        $geotype = $type;
+    
+        return $this->addColumn('geometry', $column, compact('geotype'));
+    }
+
     /**
      * Create a new binary column on the table.
      *
